567ccc896e4af0fb67fd2123fb95000af2957cf7
Elaine Anderson <eparie@bu.edu>
2018-05-03 15:59:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+moves = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+
+def display_board moves
+  puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
+end
+
