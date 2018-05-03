607da477f841f97afd7f030ee76686b453deffb7
Elaine Anderson <eparie@bu.edu>
2018-05-03 16:07:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,6 +3,6 @@
 
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
-def display_board moves
+def display_board board
   puts " #{board[0]}  |  #{board[1]} | #{board[2]}  \n-----------\n #{board[3]}  | #{board[4]}  | #{board[5]}  \n-----------\n #{board[6]}  | #{board[7]}  | #{board[8]}  \n"
 end
